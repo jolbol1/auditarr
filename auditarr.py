2c1fe08342a6fb7a6d72cf1d453f86db7fa9957a
jolbol1 <github@jamesshopland.com>
2021-06-01 18:18:51 +0100

    add comments and check root folders

--- auditarr.py
+++ auditarr.py
@@ -4,6 +4,7 @@ import datetime
 import argparse
 import logging as log
 
+# Set up the arguments for the app
 parser = argparse.ArgumentParser()
 parser.add_argument(dest="radarrdb",
                     help="Run with desired config.yml file",
@@ -23,21 +24,23 @@ parser.add_argument("-v", "--verbose",
 
 args = parser.parse_args()
 
+# Set up a logging format, allowing use of -v for debug mode
 if args.verbose:
     log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
     log.info("Verbose output.")
 else:
     log.basicConfig(format="%(levelname)s: %(message)s", level=log.INFO)
 
-
+# Gets the difference between two lists provided as parameters
 def Diff(li1, li2):
     return list(set(li1) - set(li2)) + list(set(li2) - set(li1))
 
-
-def auditPlex(radarrdb, plexdb, libraryid):
+# Function to get the folders as seen by plex and radarr, compare them and report the difference
+def auditPlex(radarrdb, plexdb, libraryid='1'):
     radarrFolders = getRadarrFolders(args.radarrdb)
     plexFolders = getPlexFolders(args.plexdb, libraryid)
     difference = Diff(radarrFolders, plexFolders)
+    # Output difference to user
     log.info("Difference:")
     for item in difference:
         if item in radarrFolders:
@@ -48,13 +51,17 @@ def auditPlex(radarrdb, plexdb, libraryid):
         else:
             log.info(item)
 
+# Get the folders from the PlexDB
 def getPlexFolders(plexdb, libraryid):
     plexFoldersAr = []
     plexDB = sqlite3.connect(plexdb)
+    # Seacrh method: First, grab all the media in the library in 'media_items', 
+    # then use this id to get the directory id from 'media_parts', 
+    # finally use the directory id to get the folder path from 'directories'
     searchMethod = "SELECT path FROM directories WHERE id IN (SELECT directory_id FROM media_parts WHERE media_item_id IN (SELECT id FROM media_items WHERE library_section_id='{}'))".format(libraryid)
-    print(searchMethod)
     plexFolders = plexDB.execute(searchMethod)
     log.debug(plexFolders)
+    # Add the results to a list and then return that list.
     for row in plexFolders:
         log.debug(row[0])
         plexFoldersAr.append(str(row[0]))
@@ -64,22 +71,27 @@ def getPlexFolders(plexdb, libraryid):
 def getRadarrFolders(radarrdb):
     radarrFoldersAr = []
     radarrDB = sqlite3.connect(radarrdb)
-    radarrFolders = radarrDB.execute("SELECT path FROM Movies WHERE MovieFileId!='0'")
     rootFolders = []
+    # First, get all the root folders in use and add to list as we need to remove these from the strings later.
     for path in radarrDB.execute("SELECT path FROM RootFolders"):
         rootFolders.append(path[0])
+    # Then get the folder paths for downloaded movies from the radarrDB
+    radarrFolders = radarrDB.execute("SELECT path FROM Movies WHERE MovieFileId!='0'")
     log.debug(radarrFolders)
+    # For each folder path remove the root folder and return list of movie folders.
     for row in radarrFolders:
         fullPath = str(row[0])
+        # Remove the root folder path from the string to give us the directory name only
         for path in rootFolders:
             log.debug("Need to remove " + str(path))
-            newPath = fullPath.replace(path, "")
-            log.debug(newPath)
-            radarrFoldersAr.append(newPath)
+            # Health check to ensure the root folder is part of the string. Allows for multiple root folders without breaking
+            if path in fullPath:
+                fullPath = fullPath.replace(path, "")
+                log.debug(fullPath)
+            radarrFoldersAr.append(fullPath)
     log.debug(radarrFoldersAr)
+    # Return the radarr folder paths with root folders stripped.
     return radarrFoldersAr
-
-
 
 auditPlex(args.radarrdb, args.plexdb, args.libraryid)
 
